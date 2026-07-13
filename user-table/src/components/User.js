c7436185bb3da20101239efc37820052f08142c8
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-07-13 16:26:20 +0200

    users map/id/username added to User.js

--- user-table/src/components/User.js
+++ user-table/src/components/User.js
@@ -21,6 +21,16 @@ const User = () => {
                     <td>Email</td>
                 </tr>
             </thead>
+            <tbody>
+                {users.map((user)  => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                  </tr>
+                ))}
+            </tbody>
         </table>
     </div>
   )
